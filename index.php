5c611267ffdaf85810c9abff76a1050bbab75c6c
PaoloTarcisio <paoloc50@gmail.com>
2024-02-14 18:13:34 +0100

    inizio esercizio

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+    <title>PHP ToDo List JSON</title>
+</head>
+<body>
+    <main id="app">
+        {{message}}
+    </main>
+
+
+
+    <!-- VUE CDN -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- AXIOS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- JS -->
+    <script src="js/scripts.js"></script>
+
+</body>
+</html>
---
+++ todos.php
@@ -0,0 +1,7 @@
+<?php
+
+
+    $listatodos = file_get_contents("db/todos.json");
+
+    
+?>
